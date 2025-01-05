f4fc1532b34f3abc93e2cc4afe68db53721291f1
refleks1a <145009608+refleks1a@users.noreply.github.com>
2025-01-05 20:58:30 +0400

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,30 +6,29 @@ const Footer = () => {
   return (
     <footer className="footer-container">
       <div className="">
-        <h2>About Us</h2>
+        <h2>Məlumat</h2>
         <ul>
-          <li>Careers</li>
-          <li>Our Stories</li>
-          <li>Terms & Conditions</li>
-          <li>Privacy Policy</li>
+          <li>Konfidensiallıq siyasəti</li>
+          <li>Qiymət siyasəti</li>
+          <li>Saytın istifadə şərtləri</li>
+          <li>Korporativ satışlar</li>
         </ul>
       </div>
       <div className="">
-        <h2>Customer Care</h2>
+        <h2>Müştərilər üçün</h2>
         <ul>
-          <li>Help Center</li>
-          <li>How To Buy</li>
-          <li>Track Your Order</li>
-          <li>Corporate & Bulk Purchasing</li>
-          <li>Returns & Refunds</li>
+          <li>Trade-in</li>
+          <li>Çatdırılma və ödəmə</li>
+          <li>Geri qaytarma siyasəti</li>
+          <li>Zəmanətlər</li>
+          <li>Naxçıvana çatdırılma</li>
         </ul>
       </div>
       <div className="">
-        <h2>Contact Us</h2>
+        <h2>Kontakt</h2>
         <ul>
           <li>12/2 Shat Masjid Road, Dhanmondi, Dhaka</li>
           <li className="contact-info-flex">
-            Email :
             <a
               target="_blank"
               href="mailto:arshadchowdhury46@gmail.com"
@@ -39,7 +38,6 @@ const Footer = () => {
             </a>
           </li>
           <li className="contact-info-flex">
-            Phone :{" "}
             <a
               target="_blank"
               href="https://api.whatsapp.com/send?phone=8801317089432"
