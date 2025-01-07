8198b85ccfbaf76e9932045ef27e079488655d99
Ilham <ilham.yaqubzada.std@bhos.edu.az>
2025-01-07 12:27:40 +0400

    Add files via upload

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,13 +6,34 @@ const Footer = () => {
   return (
     <footer className="footer-container">
       <div className="">
-        <h2>Məlumat</h2>
+        
+       <h2>Çağrı mərkəzi</h2>
+        <li style={{fontSize:'17px'}}>+994705854432</li>
+
+        <br/><h2>İş saatları</h2>
         <ul>
-          <li>Konfidensiallıq siyasəti</li>
-          <li>Qiymət siyasəti</li>
-          <li>Saytın istifadə şərtləri</li>
-          <li>Korporativ satışlar</li>
+          <li>B.e. - C. 9:00 –- 00:00</li>
+          <li>Ş - B. 10:00 -– 00:00</li>
         </ul>
+
+        
+        <br/><h2>Bizi izləyin</h2>
+        <ul class="logo-container">
+          <li class="logo-item">
+            <img src="https://umico.az/images/footer-whatsapp.svg" alt="Whatsapp logo" />
+            <span>Whatsapp</span>
+          </li>
+          <li class="logo-item">
+            <img src="https://umico.az/images/footer-instagram.svg" alt="Whatsapp logo" />
+            <span>Instagram</span>
+          </li>
+          <li class="logo-item">
+            <img src="https://umico.az/images/footer-tiktok.svg" alt="Whatsapp logo" />
+            <span>TikTok</span>
+          </li>
+        </ul>
+
+
       </div>
       <div className="">
         <h2>Müştərilər üçün</h2>
@@ -23,6 +44,16 @@ const Footer = () => {
           <li>Zəmanətlər</li>
           <li>Naxçıvana çatdırılma</li>
         </ul>
+
+        <br />
+        <h2>Məlumat</h2>
+        <ul>
+          <li>Konfidensiallıq siyasəti</li>
+          <li>Qiymət siyasəti</li>
+          <li>Saytın istifadə şərtləri</li>
+          <li>Korporativ satışlar</li>
+        </ul>
+
       </div>
       <div className="">
         <h2>Kontakt</h2>
@@ -31,7 +62,7 @@ const Footer = () => {
           <li className="contact-info-flex">
             <a
               target="_blank"
-              href="mailto:arshadchowdhury46@gmail.com"
+              href="mailto:xxxxxxxx@gmail.com"
               className="icon-flex phone-icon"
             >
               arshadchowdhury46@gmail.com
@@ -40,10 +71,10 @@ const Footer = () => {
           <li className="contact-info-flex">
             <a
               target="_blank"
-              href="https://api.whatsapp.com/send?phone=8801317089432"
+              href="https://api.whatsapp.com/send?phone=994775116975"
               className="icon-flex phone-icon"
             >
-              +8801317089432
+                +994775116975
             </a>
           </li>
           <li>
