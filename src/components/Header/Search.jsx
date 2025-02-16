d6b13f3f712dd6593880439f719164409e2d1e07
refleks1a <fhbxdfh.dhncdtb@gmail.com>
2025-02-17 00:16:15 +0100

    header.search: add search functionality

--- src/components/Header/Search.jsx
+++ src/components/Header/Search.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Link } from "react-router-dom";
 
 const Search = ({ cartItems }) => {
@@ -6,6 +6,43 @@ const Search = ({ cartItems }) => {
     const search = document.querySelector(".search");
     search.classList.toggle("active", window.scrollY > 100);
   });
+
+  const [searchText, setSearchText] = useState("");
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const [allProducts, setAllProducts] = useState([])
+
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      if(searchText.length == 0){
+        setAllProducts([])
+        setShowDropdown(false);
+      }
+    }, 500); // Adjust delay (500ms)
+
+    return () => {
+      clearTimeout(handler); // Clear timeout if user keeps typing
+    };
+  }, [searchText]);
+
+
+  const handleSearch = () => {
+    fetch("https://texnotech.store/products?search_query=" + searchText)
+      .then((response) => response.json())
+      .then((data) => {
+        const products = data.map((product) => ({
+          name: product.name,
+          id: product.id,
+        }));
+
+        if(products.length > 0) {
+          setAllProducts(products);
+          setShowDropdown(true);
+        }
+      })
+      .catch((error) => console.error("Error fetching products:", error));
+  }
 
   return (
     <>
@@ -18,9 +55,15 @@ const Search = ({ cartItems }) => {
           </div>
 
           <div className="search-box f_flex">
-            <i className="fa fa-search"></i>
-            <input type="text" placeholder="Axtarış edin..." />
-            <span>Bütün Növlər</span>
+            <i className="fa fa-search" onClick={handleSearch}/>
+            <input 
+              style={{fontSize: "15px"}}
+              value={searchText} 
+              onChange={(e) => setSearchText(e.target.value)}
+              onFocus={() => setShowDropdown(allProducts.length > 0)}
+              onBlur={() => setTimeout(() => setShowDropdown(false), 200)}
+              type="text" placeholder="Axtarış edin..."
+            />
           </div>
 
           <div className="icon f_flex width">
@@ -32,6 +75,38 @@ const Search = ({ cartItems }) => {
             </div>
           </div>
         </div>
+
+        <div>
+          {/* Dropdown for search results */}
+          {showDropdown && allProducts && (
+            <div>
+              <ul className="search-dropdown">
+                {allProducts.map((product) => {
+                  const productUrl = `/products/${product.name
+                    .toLowerCase()
+                    .replace(/ /g, '-')
+                    .replace(/[^a-z0-9-]/g, '')}-${product.id}`;
+                                    
+                  return (
+                    <Link
+                      key={product.id}
+                      to={{
+                        pathname: productUrl,
+                        state: { productId: product.id }, // Pass productId as state
+                      }}
+                    >
+                      <li  onClick={() => setSearchText(product.name)}>
+                        {product.name}
+                      </li>
+                    </Link>
+                    )
+                })}
+              </ul>
+            </div>
+            
+          )}
+        </div>
+
       </section>
     </>
   );
