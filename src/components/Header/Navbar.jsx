cec9afb30c0bb21c1da3b54b8a217ef5741110ec
ilham3169 <ilham.yaqubzada.std@bhos.edu.az>
2025-05-03 09:51:48 +0400

    middle - all products

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,57 +1,64 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
   const [mobileMenu, setMobileMenu] = useState(false);
+  const [showNavbar, setShowNavbar] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(window.scrollY);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+      if (currentScrollY > lastScrollY && currentScrollY > 100) {
+        setShowNavbar(false); // hide non-sticky parts of navbar on scroll down
+      } else {
+        setShowNavbar(true); // show non-sticky parts of navbar on scroll up
+      }
+      setLastScrollY(currentScrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [lastScrollY]);
 
   return (
     <>
-      <header className="header-container">
+      <div className="nav-link-bar">
+        <ul
+          className={
+            mobileMenu ? "nav-links-MobileMenu" : "link f_flex capitalize"
+          }
+          onClick={() => setMobileMenu(false)}
+        >
+          <li>
+            <Link
+              aria-label="Home"
+              className="link-hover"
+              to="/"
+              style={{ textDecoration: "none", color: "black", fontSize: 25}}
+            >
+              Əsas Səhifə
+            </Link>
+          </li>
+          <li>
+            <Link
+              aria-label="All Products"
+              className="link-hover"
+              to={{
+                pathname: `/all-products/-1`,
+                state: { categoryId: -1 },
+              }}
+              style={{ textDecoration: "none", color: "black" , fontSize: 25}}
+            >
+              Bütün Məhsullar
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <header className={`header-container ${showNavbar ? "show" : "hide"}`}>
         <div className="menu-items">
-          <div className="categories">
-          </div>
-          <ul
-            className={
-              mobileMenu ? "nav-links-MobileMenu" : "link f_flex capitalize"
-            }
-            onClick={() => setMobileMenu(false)}
-          >
-            <li>
-              <Link aria-label="Home" className="link-hover" to="/" 
-              style={{textDecoration: "none", color: "black"}}>
-                Əsas Səhifə
-              </Link>
-            </li>
-            <li>
-              <Link
-                aria-label="All Products"
-                className="link-hover"
-                to={{
-                  pathname: `/all-products/-1`,
-                  state: { categoryId: -1 }, // Pass productId as state
-                }}
-                style={{textDecoration: "none", color: "black"}}
-              >
-                Bütün Məhsullar
-              </Link>
-            </li>
-            {/* <li>
-              <Link aria-label="Login" className="link-hover" to="/login" 
-              style={{textDecoration: "none", color: "black"}}>
-                Giriş
-              </Link>
-            </li>
-            <li>
-              <Link
-                aria-label="Registration"
-                className="link-hover"
-                to="/registration"
-                style={{textDecoration: "none", color: "black"}}
-              >
-                Qeydiyyat
-              </Link>
-            </li> */}
-          </ul>
+          <div className="categories"></div>
         </div>
         <button
           aria-label="Menu bar"
